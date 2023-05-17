f5ac01a2bf0ce1025e29d70d549e3bc823e76aa6
amaloulahoucine <133246148+amaloulahoucine@users.noreply.github.com>
2023-05-17 17:31:34 +0200

    Update Test_laho.java

--- src/test/java/Test_laho.java
+++ src/test/java/Test_laho.java
@@ -1,14 +1,14 @@
 import com.microsoft.demo.Demo;
 import org.junit.Test;
 
-public class MyTest {
+public class MyTest2 {
     @Test
-    public void test_method_1() {
+    public void test_method_3() {
         Demo d = new Demo();
         d.DoSomething(true);
     }
 
     @Test
-    public void test_method_2() {
+    public void test_method_4() {
     }
 }
